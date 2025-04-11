7fc5e3b83d365c852269d3e9ee6bb8f4f117ed7a
Dal Lago Alto <rfvandermeer@proton.me>
2025-04-11 14:29:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import ifcopenshell
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://ifc-frontend.vercel.app"])
 
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
